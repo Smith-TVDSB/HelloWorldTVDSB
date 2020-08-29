ef5bfcff9e4ed9b20d4eded1bc312938fcf2ae13
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2020-08-29 14:50:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    /* Whatever is in the quotation marks will appear on a single line of output. */
+    System.out.println("Hello");
+    // Whatever is in the quotation marks will appear on a single line of output.
   }
 }
