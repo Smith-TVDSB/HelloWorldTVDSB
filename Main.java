ab3fd047beddd05edab14598dc52571e2a463046
Keith Smith <keith.smith@tvdsb.ca>
2020-08-29 16:52:00 +0000

    Added comment

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    /* Whatever is in the quotation marks will appear on a single line of output. */
   }
 }
